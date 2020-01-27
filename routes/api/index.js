5945a662061d2964a4edb6514ddc98e0d5c0e412
Mario Cortes <mcortes.1113@gmail.com>
2020-01-27 11:52:27 -0600

    updated api route files

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const productRoutes = require("./products");
-const userRoutes = require("./users");
-
-// Post routes
-router.use("/products", productRoutes);
-router.use("/users", userRoutes);
-
-module.exports = router;
