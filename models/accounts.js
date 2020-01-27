bdcca8093ad75f0faea15bc17741e6e86e932cb4
Mario Cortes <mcortes.1113@gmail.com>
2020-01-27 12:20:50 -0600

    added code for password functionality

--- models/accounts.js
+++ models/accounts.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const states = [
     "AK","AL","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO",
@@ -61,6 +62,32 @@ const accountSchema = new Schema({
     }
 
 });
+
+// Execute before each user.save() call
+accountSchema.pre("save", function(callback) {
+    let user = this;
+  
+    // Break out if the password hasn't changed
+    if (!user.isModified("password")) return callback();
+  
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function(err, salt) {
+      if (err) return callback(err);
+  
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) return callback(err);
+        user.password = hash;
+        callback();
+      });
+    });
+  });
+  
+  UserSchema.methods.verifyPassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+    });
+  };
 
 var account = mongoose.model("account", accountSchema);
 
