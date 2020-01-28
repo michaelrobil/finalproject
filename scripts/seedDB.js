2eb9c555fedfb21ac31edc3cd057615202761cb7
Mario Cortes <mcortes.1113@gmail.com>
2020-01-28 13:19:02 -0600

    updated seed file to include all 3 models

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,16 +1,39 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const accountsSeed = require("../fakePeople.json");
+const apptsSeed = require("../appts.json");
+const postsSeed = require("../userPosts.json")
 
 // This file empties the Posts collection and inserts the books below
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/servproject");
 
-const productsSeed = [];
-
-db.Product.remove({})
-  .then(() => db.Product.collection.insertMany(productsSeed))
+db.Accounts.remove({})
+  .then(() => db.Accounts.collection.insertMany(accountsSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data.result.n + " account records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+db.Appointments.remove({})
+  .then(() => db.Appointments.collection.insertMany(apptsSeed))
+  .then(data => {
+    console.log(data.result.n + " appointment records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+db.UserPosts.remove({})
+  .then(() => db.UserPosts.collection.insertMany(postsSeed))
+  .then(data => {
+    console.log(data.result.n + " user post records inserted!");
     process.exit(0);
   })
   .catch(err => {
