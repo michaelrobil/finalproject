edb3e8934cb74515460b52b21ed27f5dd995721c
Eric Energy <ericnrgnash@gmail.com>
2020-01-28 10:14:12 -0600

    sign up form edit

--- client/src/components/Jumbotron2/index.js
+++ client/src/components/Jumbotron2/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 700, clear: "both", padding: 40, textAlign: "center" }}
+      style={{ height: 700, clear: "both", margin: 40, textAlign: "center" }}
       className="jumbotron"
     >
       {children}
