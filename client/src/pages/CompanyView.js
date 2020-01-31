ef2a2d9c8c8565b8570071b94fc5ec72b9d9efba
Eric Energy <ericnrgnash@gmail.com>
2020-01-31 12:07:48 -0600

    profile image update

--- client/src/pages/CompanyView.js
+++ client/src/pages/CompanyView.js
@@ -28,70 +28,6 @@ const image = [
     },
 ]
 
-const tileData = [
-    {
-        img: Greentrees,
-        title: 'Image',
-        author: 'author',
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        img: Greentrees,
-        title: 'Image',
-        author: 'author',
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        img: Greentrees,
-        title: 'Image',
-        author: 'author',
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        img: Greentrees,
-        title: 'Image',
-        author: 'author',
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    },
-    {
-        title: 'Image',
-        author: 'author',
-        img: Greentrees
-    }
-];
-
-
 const useStyles = makeStyles(theme => ({
     root: {
         // width: '100%',
@@ -243,10 +179,9 @@ const useStyles = makeStyles(theme => ({
 export default function Search() {
     const classes = useStyles();
     const [appointments, setAppointments] = useState([])
-    const [pageImages, setPageImages] = useState()
     const [currentAccount, setCurrentAccount] = useState()
     const [companyImages, setcompanyImages] = useState()
-
+console.log("here" ,companyImages)
 
     const user = JSON.parse(localStorage.getItem('user'))
     const userID = user.data.user._id
@@ -254,7 +189,7 @@ export default function Search() {
     useEffect(() => {
         if (!companyImages) {
             API.getimages(user.data.user._id)
-                .then(res => setcompanyImages(res.data.postImageURL))
+                .then(res => setcompanyImages(res.data))
                 .catch(err => console.log(err));
         } else {
             console.log("we have images here")
@@ -263,7 +198,7 @@ export default function Search() {
 
     
     function loadpageimages() {
-     return companyImages ? companyImages.map(tile => (
+     return companyImages ? companyImages.postImageURL.map(tile => (
         <GridListTile key={tile.img} style={{ width: 300 }}>
             <img className={classes.img} src={tile} alt={tile.title} />
             <GridListTileBar
@@ -287,15 +222,6 @@ export default function Search() {
         })
         .catch(err => console.log(err));
     } 
-
-    function getProfile() {
-        console.log(image)
-        API.getPosts()
-            .then(res => {
-                setPageImages(res.data)
-                console.log(res.data)
-            })
-    }
 
     function getAppointments() {
         API.getAppts()
@@ -333,7 +259,7 @@ export default function Search() {
                         <div className={classes.cover}>
                             <img
                                 className={classes.coverImage}
-                                src="https://cdn.archpaper.com/wp-content/uploads/2018/09/portland_building_reconstruction-preview.jpg"
+                                src={companyImages ? companyImages.companyImageURL : Image}
                                 title="Live from space album cover"
                             />
                             <Main />
